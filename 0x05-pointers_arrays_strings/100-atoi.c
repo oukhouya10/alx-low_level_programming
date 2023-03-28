7168ca0d9b87ccc2e2bc56bf26d71ef43a135d53
oukhouya10 <mariyam.oukhouya@gmail.com>
2023-03-28 03:51:03 -0700

    Great leaders are willing to sacrifice

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer.
+ *
+ * @s: string input.
+ *
+ * Return: integer value.
+ **/
+
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int i = 0;
+	int sign = 1;
+
+	while (*(s + i))
+	{
+		if (*(s + i) == '-')
+			sign *= -1;
+		else if (*(s + i) >= '0' && *(s + i) <= '9')
+			num = (num * 10) + (*(s + i) - '0');
+		else if (num > 0)
+			break;
+		i++;
+	}
+	return (sign * num);
+}
+
+
