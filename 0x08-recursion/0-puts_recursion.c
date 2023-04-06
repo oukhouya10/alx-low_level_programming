6043dc4cda30e9d2901f46f74a40972b8e968694
oukhouya10 <mariyam.oukhouya@gmail.com>
2023-04-05 17:36:41 -0700

    She locked away a secret

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ *@s:pointer to the string
+ *Return:void
+ */
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
+
+
