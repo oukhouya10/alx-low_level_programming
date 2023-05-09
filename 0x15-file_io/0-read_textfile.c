30cadf16afbe1e0c86255258b2bfda979f33a02b
oukhouya10 <mariyam.oukhouya@gmail.com>
2023-05-09 07:30:18 -0700

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *read_textfile - function that reads a text file and
+ *prints it to the POSIX standard output
+ *@filename:the name of file
+ *@letters: the number of letters it should read and print
+ *Return: the actual number of letters it could read and print
+ *else return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_des;
+	ssize_t bytes_r, bytes_w;
+	char buff[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
+	file_des = open(filename, O_RDONLY);
+	if (file_des == -1)
+		return (0);
+	bytes_r = read(file_des, &buff[0], letters);
+	bytes_w = write(STDOUT_FILENO, &buff[0], bytes_r);
+	free(buff);
+	close(file_des);
+	return (bytes_r);
+}
