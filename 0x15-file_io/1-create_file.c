cec2d8937dbcd460385844432adeb28f2c760437
oukhouya10 <mariyam.oukhouya@gmail.com>
2023-05-09 08:44:37 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,11 @@
 #include "main.h"
+#include <fcntl.h>
 /**
- *create_file - function that creates a file.
- *@filename:is the name of the file to create
- *@texte_content:a NULL terminated string to write to the file
+ * create_file - function that creates a file.
+ * @filename: the name of the file to create
+ * @text_content: content to write to the file
  *
- *Return:-1 on failure or 1 on success.
- *
+ * Return: -1 on failure or 1 on success.
  */
 int create_file(const char *filename, char *text_content)
 {
