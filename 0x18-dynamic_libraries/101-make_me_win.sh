a155c24523661907b7ab315066fed33821e5e0e7
oukhouya10 <mariyam.oukhouya@gmail.com>
2023-11-15 06:49:26 -0800

    correction

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/oukhouya10/alx-low_level_programming/raw/master/0xx18-dynamic_libraries/libgiga.so
+wget -P /tmp https://github.com/oukhouya10/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
